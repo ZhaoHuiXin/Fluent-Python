3415e9d02fe250852e3ef8f8ac9d32731dc2f651
zhx <zhaohuixin@autoforce.net>
2018-05-22 09:39:50 +0800

    create chapter03

---
+++ chapter03/create_dict_different_method.py
@@ -0,0 +1,13 @@
+# -*- coding=utf-8 -*-
+#
+# Copyright @ 2018 HuiXin Zhao
+
+a = dict(one=1, two=2, three=3)
+b = {"one": 1, "two": 2, "three": 3}
+
+c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+
+d = dict([('two', 2), ('one', 1), ('three', 3)])
+e = dict({'three': 3, 'one': 1, 'two': 2})
+print(a == b == c == d == e)
+print(c)
