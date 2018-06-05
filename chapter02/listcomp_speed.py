e5284300c8b3ebdf647dad06d6538dda98dc8481
zhx <zhaohuixin@autoforce.net>
2018-06-05 15:36:46 +0800

    add statement

--- chapter02/listcomp_speed.py
+++ chapter02/listcomp_speed.py
@@ -1,6 +1,16 @@
 # -*- coding=utf-8 -*-
 #
 # Copyright @ 2018 HuiXin Zhao
+"""
+timeit.repeat(stmt='pass', setup='pass',
+timer=<default timer>, repeat=3, number=1000000)
+
+创建一个 Timer 实例，参数分别是 stmt（需要测量的语句或函数），
+setup（初始化代码或构建环境的导入语句），
+timer（计时函数），
+repeat（重复测量的次数），
+number（每一次测量中语句被执行的次数）
+"""
 import timeit
 
 TIMES = 10000
@@ -15,7 +25,7 @@ def non_ascii(c):
 def clock(label, cmd):
     res = timeit.repeat(cmd, setup=SETUP, number=TIMES)
     # res = timeit.timeit(cmd, setup=SETUP, number=TIMES)
-    print(label, *('{:.3f}'.format(x) for x in res))
+    print(label, *('{:.3f}'.format(x) for x in res))  # *拆包
     # print(label, *('{}'.format(res)))
 
 
